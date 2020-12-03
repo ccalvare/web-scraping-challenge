b6e27140c415c96579c3f767fc9e02982e080a0c
ccalvare <ccalvare@usc.edu>
2020-12-02 20:43:37 -0800

    homework file update

---
+++ app.py
@@ -0,0 +1,40 @@
+#MongoDB and Flask 
+#######################
+
+#Import Dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+####################################
+#PyMongo Connection
+####################################
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+####################################
+#Flask Route
+####################################
+#Routes
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+
+# Scrape Route to Import `scrape_mars.py` Script & Call `scrape` Function
+@app.route("/scrape")
+def scrapper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
+
+# Define Main Behavior
+if __name__ == "__main__":
+    app.run()
