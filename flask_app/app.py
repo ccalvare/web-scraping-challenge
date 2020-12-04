7545a0f0ea1b774411040e55f215b45a5e174947
ccalvare <ccalvare@usc.edu>
2020-12-04 13:46:44 -0800

    Update

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,7 +2,7 @@
 #######################
 
 #Import Dependencies
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
@@ -14,8 +14,7 @@ app = Flask(__name__)
 ####################################
 #PyMongo Connection
 ####################################
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 ####################################
 #Flask Route
@@ -23,18 +22,22 @@ mongo = PyMongo(app)
 #Routes
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    mars_data = mongo.db.collection.find_one()
+    return render_template("index.html", mars=mars_data)
 
 
 # Scrape Route to Import `scrape_mars.py` Script & Call `scrape` Function
 @app.route("/scrape")
-def scrapper():
-    mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return "Scraping Successful"
+def scrape():
+    #Run the Scrape
+    mars_data_info = scrape_mars.scrape_info()
+
+    #Update to Mongo
+    mongo.db.collection.update({}, mars_data_info, upsert=True)
+
+    return redirect("/")
+
 
 # Define Main Behavior
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
